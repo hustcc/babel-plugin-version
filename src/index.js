5da7ed891cefbd6da12993595d127e67a9d23b04
hustcc <i@hust.cc>
2018-11-20 17:20:34 +0800

    add ci

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-const fs = require('fs');
-const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
+'use strict';
 
-module.exports = function({ types: t }) {
+var fs = require('fs');
+var version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
+
+module.exports = function (_ref) {
+  var t = _ref.types;
+
   return {
     visitor: {
-      ReferencedIdentifier(path, state) {
-        const define = state.opts.define || '__VERSION__'; // 默认值
+      ReferencedIdentifier: function(path, state) {
+        var define = state.opts.define || '__VERSION__'; // 默认值
         if (path.node.name === define) {
           path.replaceWith(t.valueToNode(version));
         }
       }
-    },
+    }
   };
 };
